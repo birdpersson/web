a28269cebfcc7a06e6563308d33acf0b7338d265
Marko Blagojevic <blage500@gmail.com>
2020-07-25 19:09:31 +0200

    Odradjen gui na stranici homepage.js za host i za guest

--- HostREST/WebContent/app/homepage.js
+++ HostREST/WebContent/app/homepage.js
@@ -50,7 +50,7 @@ Vue.component('homepage',{
                     <h1 style="margin-top:10px;color:#35424a;" >Welcome <span id='titleEffect'>{{user.username}}</span>!</h1>
                     <hr style='background:#e8491d;height:1px;'>  
                 </div>
-                <div v-if='isAdmin'>
+                <div v-if='!isGuest'>
                     <!-- Page Content -->
                     <div class="container">
                         <div class="row">
@@ -105,8 +105,50 @@ Vue.component('homepage',{
                         </div>
                     </div>
                     <!-- /.row -->
-                    
+                </div>  <!-- !isGuest -->
+
+                <div v-if='isGuest'>
+                <!-- Page Content -->
+                    <div class="container">
+                        <div class="row">
+                            <!-- Apartments -->
+                                <div class="col-xl-3 col-md-6 mb-4">
+                                    <div class="card border-0 shadow">
+                                    <router-link style='text-decoration: none;color:#35424a;' to="/apartments"  class="nav-link" exact> 
+                                        <img src="img/apartment2.1.jpg" class="card-img-top" alt="...">
+                                        <div class="card-body text-center">
+                                        <h5 class="card-title mb-0">Apartments</h5>
+                                        <div class="card-text text-black-50">See list of all apartments</div>
+                                        </div>
+                                    </router-link>
+                                </div>
+                            </div>
+                            <!-- Reservations -->
+                            <div class="col-xl-3 col-md-6 mb-4">
+                                <div class="card border-0 shadow">
+                                    <router-link style='text-decoration: none;color:#35424a;' to="/reservations"  class="nav-link" exact>
+                                        <img src="img/reservation1.1.jpg" class="card-img-top" alt="...">
+                                        <div class="card-body text-center">
+                                        <h5 class="card-title mb-0">Reservations</h5>
+                                        <div class="card-text text-black-50">See list of all reservations</div>
+                                        </div>
+                                    </router-link>
+                                </div>
+                            </div>
+                            <!-- Profile-->
+                            <div class="col-xl-3 col-md-6 mb-4">
+                                <div class="card border-0 shadow">
+                                    <router-link style='text-decoration: none;color:#35424a;' to="/profile"  class="nav-link" exact>
+                                        <img src="img/profile3.1.jpg" class="card-img-top" alt="...">
+                                        <div class="card-body text-center">
+                                        <h5 class="card-title mb-0">Profile</h5>
+                                        <div class="card-text text-black-50">See or edit your profile</div>
+                                    </router-link>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                    <!-- /.row -->
                 </div>
             </div>
       
