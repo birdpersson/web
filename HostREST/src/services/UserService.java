a1870d589c27f65ab3049845bf223eda6367c6b0
birdpersson <aleksandarvujasin@gmail.com>
2020-09-17 14:09:08 +0200

    Clean up userService

--- HostREST/src/services/UserService.java
+++ HostREST/src/services/UserService.java
@@ -2,7 +2,6 @@ package services;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.ServletContext;
@@ -107,7 +106,6 @@ public class UserService {
 		return Response.status(Response.Status.FORBIDDEN).build();
 	}
 
-	// PROVERITI!!!!!!!
 	@GET
 	@Path("user/customers")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -122,21 +120,16 @@ public class UserService {
 		}
 		return Response.status(Response.Status.FORBIDDEN).build();
 	}
-	
-	//Pomocna metoda za vracanje onih usera koji kod hosta imaju rezervacije
-	//Kod izvucen iz metode getUsersByApartment kako bi se mogao pozivati i u metodi searchUsers za hosta.
-	public Collection<User> getUsersByApartmentId(String username){
+
+	public Collection<User> getUsersByApartmentId(String username) {
 		UserDAO userDao = (UserDAO) ctx.getAttribute("userDAO");
 		ReservationDAO reservationDao = (ReservationDAO) ctx.getAttribute("reservationDAO");
 		ApartmentDAO apartmentDao = (ApartmentDAO) ctx.getAttribute("apartmentDAO");
 		Collection<User> retVal = new ArrayList<User>();
-		
-		// Prvo pronalazimo sve stanove vezane za tog hosta...
+
 		Collection<Apartment> hostsApart = apartmentDao.findAllApartByHostId(username);
 		for (Apartment apar : hostsApart) {
 
-			// Zatim pronalazimo sve korisnike koji imaju trenutnu rezervaciju za neki stan
-			// tog hosta...
 			Collection<Reservation> reservations = reservationDao.findAllByApartmentId(apar.getId());
 			for (Reservation r : reservations) {
 				User user = userDao.findOne(r.getGuestId());
@@ -147,127 +140,62 @@ public class UserService {
 		return retVal;
 	}
 
-	//Vracanje usera spram search upita
-	// PROVERITI!!!!!!!
 	@GET
 	@Path("user/search")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response searchUsers(@Context HttpServletRequest request, 
-			@QueryParam("username") String username,
-			@QueryParam("gender") String gender,
-			@QueryParam("role") String role) {
-		
+	public Response searchUsers(@Context HttpServletRequest request, @QueryParam("username") String username,
+			@QueryParam("gender") String gender, @QueryParam("role") String role) {
+
 		String requstUsername = AuthService.getUsername(request);
 		UserDAO userDao = (UserDAO) ctx.getAttribute("userDAO");
-		
-		if(userDao.findOne(requstUsername).getRole().toString().equals("ADMIN")) {
+
+		if (userDao.findOne(requstUsername).getRole().toString().equals("ADMIN")) {
 			Collection<User> allUsers = userDao.findAll();
 			allUsers = filterUsers(allUsers, username, gender, role);
-			
+
 			return Response.status(Response.Status.OK).entity(allUsers).build();
-		}	
-		else if(userDao.findOne(requstUsername).getRole().toString().equals("HOST")) {
+		} else if (userDao.findOne(requstUsername).getRole().toString().equals("HOST")) {
 			Collection<User> allUsers = getUsersByApartmentId(requstUsername);
 			allUsers = filterUsers(allUsers, username, gender, role);
-			
+
 			return Response.status(Response.Status.OK).entity(allUsers).build();
 		}
-		
+
 		return Response.status(Response.Status.FORBIDDEN).build();
 	}
-	
-	
-	//Pomocna metoda koja sluzi za filtraciju korisnika po parametrima, 
-	//izvdojena je iz searchUser kako bi se mogla 2 put pozivati i za admina i za hosta.
-	public Collection<User> filterUsers(Collection<User> allUsers,String username, String gender, String role) {
-		if(username != null && username != "") {
+
+	public Collection<User> filterUsers(Collection<User> allUsers, String username, String gender, String role) {
+		if (username != null && username != "") {
 			Collection<User> helpUsers = new ArrayList<User>();
-			for(User u : allUsers) {
-				if(u.getUsername().toLowerCase().equals((username).toLowerCase())) {
+			for (User u : allUsers) {
+				if (u.getUsername().toLowerCase().equals((username).toLowerCase())) {
 					helpUsers.add(u);
 				}
 			}
 			allUsers = helpUsers;
 		}
-		
-		if(gender != null) {
+
+		if (gender != null) {
 			Collection<User> helpUsers = new ArrayList<User>();
-			for(User u : allUsers) {
-				if(u.getGender().toLowerCase().equals((gender).toLowerCase())) {
+			for (User u : allUsers) {
+				if (u.getGender().toLowerCase().equals((gender).toLowerCase())) {
 					helpUsers.add(u);
 				}
 			}
 			allUsers = helpUsers;
 		}
-		
-		if(role != null) {
+
+		if (role != null) {
 			Collection<User> helpUsers = new ArrayList<User>();
-			for(User u : allUsers) {
-				if(u.getRole().toString().toLowerCase().equals((role).toLowerCase())) {
+			for (User u : allUsers) {
+				if (u.getRole().toString().toLowerCase().equals((role).toLowerCase())) {
 					helpUsers.add(u);
 				}
 			}
 			allUsers = helpUsers;
 		}
-		
+
 		return allUsers;
 	}
-	
-	
-	
-//	//Vracanje usera spram search upita
-//	@GET
-//	@Path("user/search")
-//	@Produces(MediaType.APPLICATION_JSON)
-//	public Response getReviewsByHostId(@Context HttpServletRequest request, 
-//			@QueryParam("username") String username,
-//			@QueryParam("gender") String gender,
-//			@QueryParam("role") String role) {
-//		
-//		String requstUsername = AuthService.getUsername(request);
-//		UserDAO userDao = (UserDAO) ctx.getAttribute("userDAO");
-//		Collection<User> allUsers = userDao.findAll();
-//		System.out.println("username: " + username);
-//		System.out.println("gender: " + gender);
-//		System.out.println("role: " + role);
-//		if(userDao.findOne(requstUsername).getRole().toString().equals("ADMIN")) {
-//			if(username != null) {
-//				Collection<User> helpUsers = new ArrayList<User>();
-//				for(User u : allUsers) {
-//					if(u.getUsername().toLowerCase().equals((username).toLowerCase())) {
-//						helpUsers.add(u);
-//					}
-//				}
-//				allUsers = helpUsers;
-//			}
-//			
-//			if(gender != null) {
-//				Collection<User> helpUsers = new ArrayList<User>();
-//				for(User u : allUsers) {
-//					if(u.getGender().toLowerCase().equals((gender).toLowerCase())) {
-//						helpUsers.add(u);
-//					}
-//				}
-//				allUsers = helpUsers;
-//			}
-//			
-//			if(role != null) {
-//				Collection<User> helpUsers = new ArrayList<User>();
-//				for(User u : allUsers) {
-//					if(u.getRole().toString().toLowerCase().equals((role).toLowerCase())) {
-//						helpUsers.add(u);
-//					}
-//				}
-//				allUsers = helpUsers;
-//			}
-//			
-//			return Response.status(Response.Status.OK).entity(allUsers).build();
-//		}
-//		else if(userDao.findOne(requstUsername).getRole().toString().equals("HOST")) {
-//			
-//		}
-//		
-//		return Response.status(Response.Status.FORBIDDEN).build();
-//	}
 
 }
